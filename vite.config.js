421dbfc914a6a407fca9f7fa1df2453db65050dc
henri <henri@snatch-hub.com>
2025-12-16 18:49:48 +0100

    fix: hosting base to root to restore prod

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/solaire-frontend/',
+  // Firebase Hosting serves at root
+  base: '/',
   plugins: [react()],
 });
